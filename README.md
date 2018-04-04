a8eef4e33f76607b1e0e73eda65f03cdefbd9c9e
yaoandy107 <yaoandy107@gmail.com>
2018-04-04 21:26:51 +0800

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# .shconf
+My shell config, thanks for [PinLin/.shconf](https://github.com/PinLin/.shconf)
+
+## Requirements
++ `git`
++ `vim`
++ `tmux`
++ `zsh`
++ [`oh-my-zsh`](http://ohmyz.sh)
+```sh
+# oh-my-zsh installation guide
+# Install by wget
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+# Install by curl
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+```
+
+## Install
+
+```sh
+# Install by wget
+wget -qO- https://raw.githubusercontent.com/yaoandy107/.shconf/master/install.sh | bash
+
+# Install by curl
+curl -o- https://raw.githubusercontent.com/yaoandy107/.shconf/master/install.sh | bash
+```
+
+The special font-type that Powerline may need was put in the `font` directory.
---
+++ install.sh
@@ -0,0 +1,57 @@
+# set constant
+repo='.shconf'
+
+# check reqirements
+for app in git; do
+    if ! command -v $app > /dev/null; then
+        echo 'Could not find `'$app'`, installation stopped.'
+        exit 1
+    fi
+done
+
+# clone into computer
+if [ -d ~/$repo ]; then
+    rm -rf ~/$repo
+fi
+git clone https://github.com/yaoandy107/$repo ~/$repo
+if [ $? != 0 ]; then
+    echo Could not clone $repo.
+    exit 2
+fi
+
+# setup vim
+if command -v vim > /dev/null; then
+    mv ~/.vimrc ~/.vimrc.bak
+    echo "source ~/$repo/vim/sample.vimrc" >> ~/.vimrc
+else
+    echo Could not find `vim`.
+fi
+
+# setup tmux
+if command -v tmux > /dev/null; then
+    mv ~/.tmux.conf ~/.tmux.conf.bak
+    echo "source ~/$repo/tmux/sample.tmux.conf" >> ~/.tmux.conf
+else
+    echo Could not find `tmux`.
+fi
+
+# setup zsh
+if command -v zsh > /dev/null; then
+    if [ -d ~/.oh-my-zsh ]; then
+        mv ~/.zshrc ~/.zshrc.bak
+        echo "source ~/$repo/zsh/sample.zshrc" >> ~/.zshrc
+        # install zsh-autosuggestions
+        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+            git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+        fi
+        # install zsh-syntax-highlighting
+        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+        fi
+    else
+        echo Could not find `oh-my-zsh`.
+    fi
+else
+    echo Could not find `zsh`.
+fi
+
