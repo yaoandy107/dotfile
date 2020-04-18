f8e82f9674000d85b042f28178eb96010b62027a
yaoandy107 <yaoandy107@gmail.com>
2020-04-18 17:43:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,186 +1,128 @@
 #! /usr/bin/env bash
 
-NAME=".shconf"
-URL="https://github.com/yaoandy107/$NAME"
+REPO_URL="https://github.com/yaoandy107/.shconf"
+REPO_NAME=".shconf"
 
-# Install application
-function makeInstall {
-    # Check argc
-    if [ $# -ge 1 ]; then
-        # macOS
-        if [ "$(uname)" == "Darwin" ]; then
-            # Ask for install brew
-            if askQuestion "You must install brew, but do you want to install brew?" "Yn"; then
-                /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-                result=$?; if [ $result -ne 0 ]; then return $result; fi
-            brew install $1
-            return $?
-        fi
+INSTALL_DIRECTORY=${INSTALL_DIRECTORY:-"$HOME/.$REPO_NAME"}
+INSTALL_VERSION=${INSTALL_VERSION:-"master"}
 
-        # Debian / Ubuntu
-        elif command -v apt > /dev/null 2>&1; then
-            sudo apt install $1 -y
-            return $?
+askquestion() {
+    printf "$1 [y/N] "
 
-        elif command -v apt-get > /dev/null 2>&1; then
-            sudo apt-get install $1 -y
-            return $?
-
-        # Fedora / CentOS
-        elif command -v dnf > /dev/null 2>&1; then
-            sudo dnf -y install $1
-            return $?
-
-        elif command -v yum > /dev/null 2>&1; then
-            sudo yum -y install $1
-            return $?
-
-        # Embedded
-        elif command -v ipkg > /dev/null 2>&1; then
-            sudo ipkg install $1
-            return $?
-
-        elif command -v opkg > /dev/null 2>&1; then
-            sudo opkg install $1
-            return $?
-
-        # None
-        else
-            return 'QwQ'
-
-        fi
-    else
-        return -1
-    fi
+    read ans
+    case $ans in
+    [Yy*])
+        return $(true)
+        ;;
+    *)
+        return $(false)
+        ;;
+    esac
 }
 
-# Ask for question
-function askQuestion {
-    # Check argc
-    if [ $# -ge 2 ]; then
-        # default yes
-        if [ "$2" == "Yn" ]; then
-            # Display question
-            echo -n "$1 [Y/n] "
-            # Input answer
-            read ans
-            for no in 'n' 'N' 'no' 'No' 'NO' 'nO'; do
-                if [ "$ans" == "$no" ]; then ans="n"; break; fi
-            done
-        fi
-        if [ "$ans" != "n" ]; then ans="y"; fi
-        # default no
-        if [ "$2" == "yN" ]; then
-            # Display question
-            echo -n "$1 [y/N] "
-            # Input answer
-            read ans
-            for yes in 'y' 'Y' 'ye' 'Ye' 'yE' 'YE' 'yes' 'Yes' 'yEs' 'yeS' 'YEs' 'yES' 'YeS' 'YES'; do
-                if [ "$ans" == "$yes" ]; then ans="y"; break; fi
-            done
-        fi
-        if [ "$ans" != "y" ]; then ans="n"; fi
-        # result
-        [ "$ans" == "y" ]
-        return $?
-    else
-        return -1
+applyzsh() {
+    # Check zsh
+    if ! command -v zsh >/dev/null 2>&1; then
+        echo "zsh is not installed."
+        return $(false)
     fi
+
+    # Install vim
+    if ! [ -d ${ZSH_CUSTOM:-$HOME/.zim/zimfw.zsh} ]; then
+        export RUNZSH=no
+
+        if command -v curl >/dev/null 2>&1; then
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh)"
+        else
+            sh -c "$(wget -nv -O - https://raw.githubusercontent.com/zimfw/install/master/install.zsh)"
+        fi
+    fi
+    # Install powerlevel10k
+    if ! [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]; then
+        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    fi
+    # Install zsh-autosuggestions
+    if ! [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    fi
+    # Install zsh-syntax-highlighting
+    if ! [ -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    fi
+
+    if [ -f $HOME/.zshrc ]; then
+        mv $HOME/.zshrc $HOME/.zshrc.bak
+    fi
+    echo "export DOTFILES=$INSTALL_DIRECTORY" >>$HOME/.zshrc
+    echo "source $INSTALL_DIRECTORY/zsh/.zshrc" >>$HOME/.zshrc
+    echo "DEFAULT_USER=$USER" >>$HOME/.zshrc
 }
 
-function main {
-    # Require git
-    if ! command -v git > /dev/null 2>&1; then
-        # Ask for install git
-        if askQuestion "You must install git, but do you want to install git?" "Yn"; then
-            makeInstall git
-            result=$?; if [ $result -ne 0 ]; then return $result; fi
-        else
-            return 0
-        fi
+applyvim() {
+    # Check vim
+    if ! command -v vim >/dev/null 2>&1; then
+        echo "vim is not installed."
+        return $(false)
     fi
 
-    # Remove local repo if exist
-    if [ -d ~/$NAME ]; then
-        rm -rf ~/$NAME
+    if [ -f $HOME/.vimrc ]; then
+        mv $HOME/.vimrc $HOME/.vimrc.bak
+    fi
+    echo "source $INSTALL_DIRECTORY/vim/.vimrc" >>$HOME/.vimrc
+}
+
+applytmux() {
+    # Check tmux
+    if ! command -v tmux >/dev/null 2>&1; then
+        echo "tmux is not installed."
+        return $(false)
     fi
 
-    # Clone repo
-    git clone $URL ~/$NAME
+    if [ -f $HOME/.tmux.conf ]; then
+        mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
+    fi
+    echo "source $INSTALL_DIRECTORY/tmux/.tmux.conf" >>$HOME/.tmux.conf
+}
+
+main() {
+    # Check Git
+    if ! command -v git >/dev/null 2>&1; then
+        echo "You must install git before using the installer."
+        return $(false)
+    fi
+
+    # Remove old one
+    if [ -d $INSTALL_DIRECTORY ]; then
+        rm -rf $INSTALL_DIRECTORY
+    fi
+
+    # Clone repo to local
+    git clone $REPO_URL $INSTALL_DIRECTORY
     if [ $? != 0 ]; then
-        echo "Could not  clone$NAME."
+        echo "Failed to clone $REPO_NAME."
         return 1
     fi
+    cd $INSTALL_DIRECTORY
+    git checkout $INSTALL_VERSION
 
-    # Require zsh
-    if ! command -v zsh > /dev/null 2>&1; then
-        # Ask for install zsh
-        if askQuestion "You must install zsh, but do you want to install zsh?" "Yn"; then
-            makeInstall zsh
-            result=$?; if [ $result -ne 0 ]; then return $result; fi
-        else
-            return 0
-        fi
-    fi
-    # Config zsh
-    if command -v zsh > /dev/null 2>&1; then
-        # Require oh-my-zsh
-        if ! [ -d ~/.oh-my-zsh ]; then
-            sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh//g')" || \
-            sh -c "$(wget -qO- https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sed 's/env zsh//g')"
-        fi
-        # Install zsh-autosuggestions
-        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]; then
-            git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-        fi
-        # Install zsh-syntax-highlighting
-        if ! [ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]; then
-            git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-        fi
-        if [ -f ~/.zshrc ]; then
-            mv ~/.zshrc ~/.zshrc.bak
-        fi
-        if ! [ -f  ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
-            git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-        fi
-        echo "source ~/$NAME/config/zsh/sample.zshrc" >> ~/.zshrc
+    # Apply the config of zsh
+    if askquestion "Do you want to apply the config of zsh?"; then
+        applyzsh
     fi
 
-    # Check vim
-    if ! command -v vim > /dev/null 2>&1; then
-        # Ask for install vim
-        if askQuestion "Do you want to install vim?" "yN"; then
-            makeInstall vim
-            result=$?; if [ $result -ne 0 ]; then return $result; fi
-        fi
-    fi
-    # Config vim
-    if command -v vim > /dev/null 2>&1; then
-        if [ -f ~/.vimrc ]; then
-            mv ~/.vimrc ~/.vimrc.bak
-        fi
-        echo "source ~/$NAME/config/vim/sample.vimrc" >> ~/.vimrc
+    # Apply the config of vim
+    if askquestion "Do you want to apply the config of vim?"; then
+        applyvim
     fi
 
-    # Check tmux
-    if ! command -v tmux > /dev/null 2>&1; then
-        # Ask for install tmux
-        if askQuestion "Do you want to install tmux?" "yN"; then
-            makeInstall tmux
-            result=$?; if [ $result -ne 0 ]; then return $result; fi
-        fi
-    fi
-    # Config tmux
-    if command -v tmux > /dev/null 2>&1; then
-        if [ -f ~/.tmux.conf ]; then
-            mv ~/.tmux.conf ~/.tmux.conf.bak
-        fi
-        echo "source ~/$NAME/config/tmux/sample.tmux.conf" >> ~/.tmux.conf
+    # Apply the config of tmux
+    if askquestion "Do you want to apply the config of tmux?"; then
+        applytmux
     fi
 
     # Finished
     echo
-    echo Done! $NAME was installed.
+    echo "Done! $REPO_NAME:$INSTALL_VERSION is ready to go! Restart your shell to use it."
 }
 
 main
